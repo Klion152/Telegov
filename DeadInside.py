9b0f2d357e9c97dcea42d725ca7bea1fb1174147
Klion <78539219+Klion152@users.noreply.github.com>
2021-02-10 09:59:29 +0500

    Add files via upload

---
+++ DeadInside.py
@@ -0,0 +1,11 @@
+import random
+a = random.randint(1,10)
+print(a)
+while a == aa :
+    aa = int(input("Число"))
+    print(aa)
+    if a == aa :
+        print("Верно")
+        break
+    else :
+        print("Неверно")
